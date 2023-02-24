e7e94fecf8d7a82cfa5df4d9a898095aa03c1a43
Kantanat Siripipatworakun <phantomktn@gmail.com>
2023-02-24 22:19:42 +0700

    first commit of profile vue

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="`pa-2 border-${color}`" style={style}>
+    <h3 v-if="title" :class="`txt-${color} text-center`">{{title}}</h3>
+    <slot />
+  </div>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    color: String,
+    title: String,
+    borderRadius: String,
+    borderColor: String,
+    backgroundColor: String,
+    padding: String,
+    margin: String,
+    width: String,
+    height: String
+  },
+}
+</script>
